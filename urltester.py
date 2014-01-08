204e580eb15fca64b75878f862df90f32d26f751
Greg Silverstein <greg.silverstein@gmail.com>
2014-01-07 19:56:56 -0500

    initial commit

---
+++ urltester.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2.7
+
+"""
+This python class
+
+
+"""
+
+import Queue
+import threading
+import urllib2
+
+# called by each thread
+def get_url(q, url):
+    q.put(urllib2.urlopen(url).read())
+
+theurls = '''http://google.com http://yahoo.com'''.split()
+
+q = Queue.Queue()
+
+for u in theurls:
+    t = threading.Thread(target=get_url, args = (q,u))
+    t.daemon = True
+    t.start()
+
+s = q.get()
+print s
